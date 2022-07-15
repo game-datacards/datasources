8850e483153d61be7a7fb34358a1609c18307681
Ron Planken <ron.planken@gmail.com>
2022-07-15 15:45:17 +0200

    Keywords updated

--- 40k/convert_csv.js
+++ 40k/convert_csv.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const readCsv = async (file) => {
+const readCsv = async (file, cleanTags = true) => {
   if (!file) {
     return;
   }
@@ -14,7 +14,15 @@ const readCsv = async (file) => {
       const headers = allRows[0].split('|').slice(0, -1).filter( h => h);
       const obj = {};
       row.split('|').slice(0, -1).forEach((val, i) => {
-        obj[headers[i]] = val.replace("'", '').replace(/(<([^>]+)>)/gi, '');
+        val = val.replace("'", '');
+
+        if(cleanTags) {
+          val = val.replace(/(<([^>]+)>)/gi, '');
+        } else {
+          val = val.replace("<", '&#60;').replace(">", '&#62;');
+        }
+
+        obj[headers[i]] = val;
       });
       return obj;
     })
@@ -22,14 +30,18 @@ const readCsv = async (file) => {
     .map( (row) => row)
     .slice(0,-1);
 
-  fs.writeFileSync(`40k/json/${file.replace('.csv', '')}.json`, JSON.stringify(res, null, 2));
+  fs.writeFileSync(`./json/${file.replace('.csv', '')}.json`, JSON.stringify(res, null, 2));
 };
 
 fs.readdir("./", function (err, files) {
 
   files.forEach(function (file) {
     if(file.indexOf('.csv') > -1) {
-      readCsv(file);
+      if(file === "Datasheets_keywords.csv") {
+        readCsv(file, false);
+      } else {
+        readCsv(file);
+      }
     }
   })
 });
