88e3cae015349b086a18a181aa5544e4ca4898c6
Ron Planken <ron.planken@gmail.com>
2023-06-09 14:00:09 +0200

    Added extracting of back of page

---
+++ 10th/conversion.helpers.js
@@ -0,0 +1,205 @@
+const getKeywords = (lines, searchText) => {
+  for (const [_index, line] of lines.entries()) {
+    if (line.includes(searchText)) {
+      return line
+        .substring(line.indexOf(searchText) + searchText.length)
+        .split(',')
+        .map((val) => val.trim());
+    }
+  }
+  return [];
+};
+const getInvulValue = (lines) => {
+  for (const [_index, line] of lines.entries()) {
+    if (line.includes('INVULNERABLE SAVE')) {
+      return line.substring(line.indexOf('INVULNERABLE SAVE') + 'INVULNERABLE SAVE'.length).trim();
+    }
+  }
+  return '';
+};
+
+const getFactionName = (lines) => {
+  let factionName = '';
+  let startOfFaction = 0;
+  for (const [_index, line] of lines.entries()) {
+    if (startOfFaction > 0) {
+      let factionLine = line.substring(startOfFaction);
+
+      if (factionLine.length === 0) {
+        continue;
+      }
+
+      factionName = factionName + ' ' + factionLine.trim();
+    }
+    if (line.includes('FACTION KEYWORDS:')) {
+      startOfFaction = line.indexOf('FACTION KEYWORDS:');
+    }
+  }
+
+  return factionName.trim();
+};
+
+const getUnitComposition = (lines) => {
+  let value = '';
+  let startOfBlock = 0;
+  for (const [_index, line] of lines.entries()) {
+    if (line.includes('LEADER') || line.includes('FACTION KEYWORDS') || line.includes('TRANSPORT') ) {
+      break;
+    }
+    if (startOfBlock > 0) {
+      let textLine = line.substring(startOfBlock).trim();
+
+      if (textLine.length === 0) {
+        continue;
+      }
+      if (textLine.trim().length > 0) {
+        value = value + ' ' + textLine.trim();
+      }
+    }
+    if (line.includes('UNIT COMPOSITION')) {
+      startOfBlock = line.indexOf('UNIT COMPOSITION');
+    }
+  }
+
+  return value.trim();
+};
+
+const getLeader = (lines) => {
+  let value = '';
+  let startOfBlock = 0;
+  for (const [_index, line] of lines.entries()) {
+    if (line.includes('FACTION KEYWORDS') || line.includes('TRANSPORT')) {
+      break;
+    }
+    if (startOfBlock > 0) {
+      let textLine = line.substring(startOfBlock).trim();
+
+      if (textLine.length === 0) {
+        continue;
+      }
+
+      if (textLine.trim().length > 0) {
+        value = value + ' ' + textLine.trim();
+      }
+    }
+    if (line.includes('LEADER')) {
+      startOfBlock = line.indexOf('LEADER');
+    }
+  }
+
+  return value.trim();
+};
+
+const getWargear = (lines) => {
+  let value = '';
+  let startOfBlock = 0;
+  let endOfBlock = 0;
+
+  for (const [_index, line] of lines.entries()) {
+    if (line.includes('FACTION KEYWORDS')) {
+      break;
+    }
+    if (startOfBlock > 0) {
+      let textLine = line.substring(startOfBlock, endOfBlock).trim();
+
+      if (textLine.length === 0) {
+        continue;
+      }
+      value = value + ' ' + textLine;
+    }
+    if (line.includes('WARGEAR OPTIONS')) {
+      startOfBlock = line.indexOf('WARGEAR OPTIONS');
+      endOfBlock = line.indexOf('UNIT COMPOSITION') - startOfBlock;
+    }
+  }
+
+  return value.trim();
+};
+
+const getTransport = (lines) => {
+  let value = '';
+  let startOfBlock = 0;
+
+  for (const [_index, line] of lines.entries()) {
+    if (line.includes('FACTION KEYWORDS')) {
+      break;
+    }
+    if (startOfBlock > 0) {
+      let textLine = line.substring(startOfBlock).trim();
+
+      if (textLine.length === 0) {
+        continue;
+      }
+      value = value + ' ' + textLine;
+    }
+    if (line.includes('TRANSPORT')) {
+      startOfBlock = line.indexOf('TRANSPORT');
+    }
+  }
+
+  return value.trim();
+};
+
+const getStartOfBlock = (lines, block) => {
+  for (const [index, line] of lines.entries()) {
+    if (line.includes(block)) {
+      return { line: index, pos: line.indexOf(block) };
+    }
+  }
+  return { line: 0, pos: 0 };
+};
+const getWeaponEndline = (lines) => {
+  for (const [index, line] of lines.entries()) {
+    if (line.includes('KEYWORDS:')) {
+      return index;
+    }
+  }
+  return 0;
+};
+
+const getStartOfWeaponsBlock = (lines, block) => {
+  for (const [index, line] of lines.entries()) {
+    if (line.includes(block)) {
+      return { line: index, pos: line.indexOf('RANGE '), endLine: lines.length };
+    }
+  }
+  return { line: 0, pos: 0 };
+};
+
+const getUnitKeywords = (lines, startOfAbilities) => {
+  for (const [index, l] of lines.entries()) {
+    const line = l.substring(0, startOfAbilities.pos);
+    if (line.includes('KEYWORDS:')) {
+      let keyWordsLine = line.substring(line.indexOf('KEYWORDS:') + 'KEYWORDS:'.length).trim();
+      if (keyWordsLine.substring(keyWordsLine.length - 1) === ',') {
+        keyWordsLine =
+          keyWordsLine + lines[index + 1].substring(line.indexOf('KEYWORDS:') + 1, startOfAbilities.pos).trim();
+      }
+      return keyWordsLine.split(',').map((val) => val.trim().replace('\x07', ''));
+    }
+  }
+  return { line: 0, pos: 0 };
+};
+
+const getName = (name) => {
+  const nameArray = name.split(' ');
+  for (var i = 0; i < nameArray.length; i++) {
+    nameArray[i] = nameArray[i].charAt(0).toUpperCase() + nameArray[i].slice(1);
+  }
+  return nameArray.join(' ');
+};
+
+module.exports = {
+  getKeywords,
+  getInvulValue,
+  getUnitKeywords,
+  getStartOfWeaponsBlock,
+  getStartOfBlock,
+  getFactionName,
+  getWeaponEndline,
+  getName,
+  getUnitComposition,
+  getLeader,
+  getWargear,
+  getTransport,
+};
