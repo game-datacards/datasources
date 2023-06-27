65dec183b51d1520e0db61df350c479c71923c99
Sherbrooke Andrews <sherbrooke@grablabs.com>
2023-06-27 17:08:52 -0400

    adding stratagem data

---
+++ 10th/stratagems/CONSTANTS.mjs
@@ -0,0 +1,21 @@
+export const TURN = {
+  either: 'either',
+  your: 'your',
+  opponents: 'opponents'
+};
+
+export const TYPE = {
+  'Battle Tactic': 'Battle Tactic',
+  'Epic Deed': 'Epic Deed',
+  'Strategic Ploy': 'Strategic Ploy',
+  Wargear: 'Wargear',
+};
+
+export const PHASE = {
+  any: 'any',
+  command: 'command',
+  movement: 'movement',
+  shooting: 'shooting',
+  charge: 'charge',
+  fight: 'fight',
+};
---
+++ 10th/stratagems/index.mjs
@@ -0,0 +1,10 @@
+import core from './core';
+import adeptuscustodes from './adeptuscustodes.mjs';
+import space_marines from './space_marines.mjs';
+
+export const data = {
+  core,
+  // key names should match the file names for generated json.gdc (with the exception of core above)
+  adeptuscustodes,
+  space_marines,
+}
