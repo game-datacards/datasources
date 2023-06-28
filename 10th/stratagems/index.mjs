13f6f5bc567cec4a33b1bf05d3ae0e33e2a76a5c
Sherbrooke Andrews <sherbrooke@grablabs.com>
2023-06-28 10:02:27 -0400

    fixing imports

--- 10th/stratagems/index.mjs
+++ 10th/stratagems/index.mjs
@@ -1,10 +1,12 @@
-import core from './core';
+import core from './core.mjs';
 import adeptuscustodes from './adeptuscustodes.mjs';
 import space_marines from './space_marines.mjs';
 
-export const data = {
+const data = {
   core,
   // key names should match the file names for generated json.gdc (with the exception of core above)
   adeptuscustodes,
   space_marines,
 }
+
+export default data;
