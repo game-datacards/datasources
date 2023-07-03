c3d9a60e88fd8980400a5727047ec11acfae4731
Ron Planken <ron.planken@gmail.com>
2023-07-03 10:44:51 +0200

    Added BT enhancements

--- 10th/convert.mjs
+++ 10th/convert.mjs
@@ -1,106 +1,12 @@
 import fs from 'fs';
 import pdfToText from 'pdf-to-text';
 
-// for (let index = 7; index < 95; index++) {
-//   if (index % 2 === 1) {
-//     const options = { from: index, to: index + 1 };
-
-//     pdfToText.pdfToText('./tyranids_index.pdf', options, function (err, data) {
-//       if (err) throw err;
-//       let text = data.toString('utf8').replaceAll('', '---PAGE 2---\n\r');
-//       fs.writeFileSync(`./tyranids/tyranids-${index}.text`, text);
-//     });
-//   }
-// }
-
-// pdfToText.info('./marines_leviathan.pdf', function (err, data) {
-//   if (err) throw err;
-//   for (let index = 1; index < data.pages; index++) {
-//     if (index % 2 === 1) {
-//       const options = { from: index, to: index + 1 };
-
-//       pdfToText.pdfToText('./marines_leviathan.pdf', options, function (err, data) {
-//         if (err) throw err;
-//         let text = data.toString('utf8').replaceAll('', '---PAGE 2---\n\r');
-//         fs.writeFileSync(`./marines_leviathan/marines_leviathan-${index}.text`, text);
-//       });
-//     }
-//   }
-// });
-// pdfToText.info('./spacemarines_index.pdf', function (err, data) {
-//   if (err) throw err;
-//   for (let index = 6; index < data.pages; index++) {
-//     if (index % 2 === 1) {
-//       const options = { from: index, to: index + 1,  };
-
-//       pdfToText.pdfToText('./spacemarines_index.pdf', options, function (err, data) {
-//         if (err) throw err;
-//         let text = data.toString('utf8').replaceAll('', '---PAGE 2---\n\r');
-//         fs.writeFileSync(`./spacemarines/spacemarines_index-${index}.text`, text);
-//       });
-//     }
-//   }
-// });
-// pdfToText.info('./blacktemplar_index.pdf', function (err, data) {
-//   if (err) throw err;
-//   for (let index = 6; index < data.pages; index++) {
-//     if (index % 2 === 1) {
-//       const options = { from: index, to: index + 1 };
-
-//       pdfToText.pdfToText('./blacktemplar_index.pdf', options, function (err, data) {
-//         if (err) throw err;
-//         let text = data.toString('utf8').replaceAll('', '---PAGE 2---\n\r');
-//         fs.writeFileSync(`./blacktemplar/blacktemplar_index.pdf-${index}.text`, text);
-//       });
-//     }
-//   }
-// });
-// pdfToText.info('./bloodangels_index.pdf', function (err, data) {
-//   if (err) throw err;
-//   for (let index = 6; index < data.pages; index++) {
-//     if (index % 2 === 1) {
-//       const options = { from: index, to: index + 1 };
-
-//       pdfToText.pdfToText('./bloodangels_index.pdf', options, function (err, data) {
-//         if (err) throw err;
-//         let text = data.toString('utf8').replaceAll('', '---PAGE 2---\n\r');
-//         fs.writeFileSync(`./bloodangels/bloodangels_index.pdf-${index}.text`, text);
-//       });
-//     }
-//   }
-// });
-// pdfToText.info('./spacewolves_index.pdf', function (err, data) {
-//   if (err) throw err;
-//   for (let index = 6; index < data.pages; index++) {
-//     if (index % 2 === 1) {
-//       const options = { from: index, to: index + 1 };
-
-//       pdfToText.pdfToText('./spacewolves_index.pdf', options, function (err, data) {
-//         if (err) throw err;
-//         let text = data.toString('utf8').replaceAll('', '---PAGE 2---\n\r');
-//         fs.writeFileSync(`./spacewolves/spacewolves_index.pdf-${index}.text`, text);
-//       });
-//     }
-//   }
-// });
-// pdfToText.info('./darkangels_index.pdf', function (err, data) {
-//   if (err) throw err;
-//   for (let index = 6; index < data.pages; index++) {
-//     if (index % 2 === 1) {
-//       const options = { from: index, to: index + 1 };
-
-//       pdfToText.pdfToText('./darkangels_index.pdf', options, function (err, data) {
-//         if (err) throw err;
-//         let text = data.toString('utf8').replaceAll('', '---PAGE 2---\n\r');
-//         fs.writeFileSync(`./darkangels/darkangels_index.pdf-${index}.text`, text);
-//       });
-//     }
-//   }
-// });
-
 function extractPDF(name, max = 0) {
   if (fs.existsSync(`./${name}_index.pdf`)) {
-    console.log('Found index, extracting sheets.');
+    if (!fs.existsSync(`./${name}`)) {
+      fs.mkdirSync(`./${name}`);
+    }
+    console.log(`Found index, extracting sheets. ${name}`);
     pdfToText.info(`./${name}_index.pdf`, function (err, data) {
       if (err) throw err;
       if (max === 0) {
@@ -157,29 +63,34 @@ function extractPDF(name, max = 0) {
   }
 }
 
-// // extractPDF('deathwatch');
-// // extractPDF('deathguard');
-// // extractPDF('thousandsons');
-// // extractPDF('worldeaters');
-// // extractPDF('chaosdaemons');
-// // extractPDF('chaos_spacemarines');
+// extractPDF('deathwatch');
+// extractPDF('deathguard');
+// extractPDF('thousandsons');
+// extractPDF('worldeaters');
+// extractPDF('chaosdaemons');
+// extractPDF('chaos_spacemarines');
 // extractPDF('chaosknights');
 // extractPDF('spacemarines');
 
-// // // extractPDF('titanlegions');
+// extractPDF('bloodangels');
+// extractPDF('darkangels');
+// extractPDF('blacktemplar');
+// extractPDF('spacewolves');
+
+// // extractPDF('titanlegions');
 // extractPDF('greyknights');
-// // extractPDF('adeptasororitas');
-// // extractPDF('adeptusmechanicus');
+// extractPDF('adeptasororitas');
+// extractPDF('adeptusmechanicus');
 // extractPDF('adeptuscustodes');
-// // extractPDF('agents');
-extractPDF('astramilitarum');
+// extractPDF('agents');
+// extractPDF('astramilitarum');
 // extractPDF('imperialknights', 29);
 
 // extractPDF('tau');
 // extractPDF('aeldari');
 // extractPDF('drukhari');
 // extractPDF('necrons');
-// extractPDF('tyranids');
-// // extractPDF('gsc');
+extractPDF('tyranids');
+// extractPDF('gsc');
 // extractPDF('orks');
-// // extractPDF('votann');
+// extractPDF('votann');
